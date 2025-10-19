0689d205682ccaaf4d5024a8d3398c2a3d493e83
lakshanjayalath <iamdhananjaya119@gmail.com>
2025-10-19 16:22:25 +0530

    Add AuthController and User model files; update assembly informational version

--- obj/Debug/net9.0/quotation-generator-back-end.AssemblyInfo.cs
+++ obj/Debug/net9.0/quotation-generator-back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("quotation-generator-back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d372a0856571c2ba2298a28f2502c38458ad09ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("quotation-generator-back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("quotation-generator-back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
