a41b33603713420d4d33c5e9d381dd6d0de81d35
lakshanjayalath <iamdhananjaya119@gmail.com>
2025-12-04 23:18:42 +0530

    fix: Update AssemblyInformationalVersion and cache inputs

--- obj/Debug/net9.0/quotation-generator-back-end.AssemblyInfo.cs
+++ obj/Debug/net9.0/quotation-generator-back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("quotation-generator-back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3eb2e43cee57a2596a700b4aabc6cba6f4cf856")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87319ace60ad8460260a7723b4b5df5cf4fc835d")]
 [assembly: System.Reflection.AssemblyProductAttribute("quotation-generator-back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("quotation-generator-back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
